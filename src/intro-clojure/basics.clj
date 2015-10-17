777209b198d1829a1f80da92dc59f4c77a25ba50
francesc <francesc.guillen@gmail.com>
2015-10-17 10:01:08 +0200

    add reader forms > literals

--- src/intro-clojure/basics.clj
+++ src/intro-clojure/basics.clj
@@ -1,1 +1,32 @@
 (ns intro-clojure.basics)
+
+(comment
+  ; Reader Forms
+
+  ;; Literals
+  ;;; strings
+  "hello world"
+  ;; characters
+  \h
+  \newline
+  ;;; numbers
+  ;;; integers
+  42
+  123412341234123412341234N
+  ;;; decimals
+  42.42
+  123412431234.12431243213M
+  ;;; rational
+  3/4
+  ;;; booleans
+  true
+  false
+  ;;; nothing, null
+  nil
+  ;;; keywords
+  :keyword
+  :color
+  :key1
+
+
+  )
