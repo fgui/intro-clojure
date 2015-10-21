4cac7d91eb010c59b5f612b21e0d5e4910d1fc9b
francesc <francesc.guillen@gmail.com>
2015-10-21 22:23:05 +0200

    auto inc add-slide

--- src/ic/lv.clj
+++ src/ic/lv.clj
@@ -3,8 +3,11 @@
 (def current-slide (atom 1))
 (def slides (atom {}))
 
-(defn add-slide [key text]
-  (swap! slides assoc key text))
+(defn add-slide
+  ([key text]
+   (swap! slides assoc key text))
+  ([text]
+   (add-slide (inc (apply max (keys @slides))) text )))
 
 (def sep-slide "----------------------------------------")
 
@@ -13,7 +16,7 @@
   ([key] (println sep-slide)
    (println (@slides key))
    (println sep-slide)
-   (str "[slide:" key "]")) )
+   (str "[slide:" key "]")))
 
 (defn next-slide []
   (swap! current-slide inc)
@@ -34,18 +37,18 @@ example inc (3) becomes (inc 3)")
          (inc (inc (3)))
          )
 
-(add-slide 2 "If first element is not an operator
+(add-slide "If first element is not an operator
 -> error")
 
-(comment 2
+(comment
          ("text" 1 3)
          )
 
-(add-slide 3 "With prefix notation
+(add-slide "With prefix notation
 Functions can accept different number of arguments.
 Arity is strict. Wrong arity -> Exception")
 
-(comment 3
+(comment
          (+ 1 3 4);
          (+ 1 3)
          (+)
@@ -55,61 +58,61 @@ Arity is strict. Wrong arity -> Exception")
          (inc 2 4)
          )
 
-(add-slide 4 "Nested prefix notation ->
+(add-slide "Nested prefix notation ->
 no need for op preference
 2 * 3 + 4 or 4 + 2 * 3 -> (+ 4 (* 2 3))")
 
-(comment 4
+(comment
          (+ 4 (* 2 3)))
 
-(add-slide 5 "Evaluation of expressions
+(add-slide "Evaluation of expressions
 Recursive -> First evaluate sub-expressions
 (+ (* 2 3) (- 5 1))
 -> (+ 6 (- 5 1))
 -> (+ 6 4)
 -> 10")
 
-(comment 5
+(comment
          (+ (* 2 3) (- 5 1))
          )
 
-(add-slide 6 "Naming Things
+(add-slide "Naming Things
 with def")
 
-(comment 6
+(comment
          "I lied a bit went I explained that (function...) special form"
          (def answer-to-everything 42)
          "def is a special for that associates a name to a value"
          )
 
-(add-slide 7 "doing more than one expression
+(add-slide "doing more than one expression
 with do.
 main use -> side effects
 returns last value of expression")
 
-(comment 7
+(comment
          ((println "hello") (println "bye"))
          (do (println "hello") (println "bye"))
 
          )
 
-(add-slide 8 "let it be... local names")
+(add-slide "let it be... local names")
 
-(comment 8
+(comment
         (def x 10)
         (let [x 2] (* x x))
         (inc x)
         )
 
-(add-slide 9 "from let to function fn")
+(add-slide "from let to function fn")
 
-(comment 9
+(comment
          (fn [x] (* x x))
          #(* %1 %1)
          )
 
-(add-slide 10 "naming a function")
+(add-slide  "naming a function")
 
-(comment 10
+(comment
          (def square (fn [x] (* x x)))
          (defn square [x] (* x x)))
