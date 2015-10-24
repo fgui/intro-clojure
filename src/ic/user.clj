363d090acc728aeed0d10033fd3dfe90a6908a01
francesc <francesc.guillen@gmail.com>
2015-10-24 18:11:49 +0200

    types simple + collections

--- src/ic/user.clj
+++ src/ic/user.clj
@@ -30,40 +30,40 @@
 -> (function param-1 param-2 ... param-n)
 example inc (3) becomes (inc 3)")
 
-(comment 1
-         ;; inc (3)
-         (inc 3)
-         ;; inc (inc (3))
-         (inc (inc (3)))
-         )
+(comment
+  ;; inc (3)
+  (inc 3)
+  ;; inc (inc (3))
+  (inc (inc (3)))
+  )
 
 (add-slide "If first element is not an operator
 -> error")
 
 (comment
-         ("text" 1 3)
-         )
+  ("text" 1 3)
+  )
 
 (add-slide "With prefix notation
 Functions can accept different number of arguments.
 Arity is strict. Wrong arity -> Exception")
 
 (comment
-         (+ 1 3 4);
-         (+ 1 3)
-         (+)
-         +
-         (source +)
-         ;; error ArityException
-         (inc 2 4)
-         )
+  (+ 1 3 4)
+  (+ 1 3)
+  (+)
+  +
+  (source +)
+  ;; error ArityException
+  (inc 2 4)
+  )
 
 (add-slide "Nested prefix notation ->
 no need for op preference
 2 * 3 + 4 or 4 + 2 * 3 -> (+ 4 (* 2 3))")
 
 (comment
-         (+ 4 (* 2 3)))
+  (+ 4 (* 2 3)))
 
 (add-slide "Evaluation of expressions
 Recursive -> First evaluate sub-expressions
@@ -73,17 +73,84 @@ Recursive -> First evaluate sub-expressions
 -> 10")
 
 (comment
-         (+ (* 2 3) (- 5 1))
-         )
+  (+ (* 2 3) (- 5 1))
+  )
 
+
+(add-slide "Basic/Simple types.
+- numbers 2 2N 1.2 1.2M 3/4
+- \"strings\", \\c \\h \\a \\r \\s
+- boolean true false
+- nil
+- :keyword
+- symbols (vars)")
+
+(comment
+  ;; Reader Forms (forms read by reader before eval)
+  ;; Literals
+  ;; strings
+  "hello world"
+  ;; characters
+  \h
+  \newline
+  ;; numbers
+  ;; integers
+  42
+  123412341234123412341234N
+  ;; decimals
+  42.42
+  123412431234.12431243213M
+  ;; rational
+  3/4
+  ;; booleans
+  true
+  false
+  ;; nothing, null
+  nil
+  ;; keywords
+  :keyword
+  :color
+  :key1
+  ;; Symbols (identifiers)
+  inc
+  +
+  hola
+  )
+
+(add-slide "Collection/Containers of types
+Clojure literals for all of them
+- List (1 \"three\" \a)
+- Vector []
+- Map {:key 123}
+- Set #{\\a 2 2/3}")
+
+(comment
+  ;; Lists
+  ;; "," are not used do  separate elements -> white spaces instead.
+  ()
+  '(1 2 3)
+  ;; Vectors
+  []
+  [1 2 3]
+  [1 "hello" 1/4]
+  ;; Maps
+  {}
+  {:key1 "value1" :key2 "value2"}
+  ;; Sets
+  #{}
+  #{1 2 3 4}
+
+  ;; they can be nested
+  [1 2 [3 4] {:one 1}]
+  )
 (add-slide "Naming Things
 with def")
 
 (comment
-         "I lied a bit went I explained that (function...) special form"
-         (def answer-to-everything 42)
-         "def is a special for that associates a name to a value"
-         )
+  "I lied a bit went I explained that (function...) special form"
+  (def answer-to-everything 42)
+  "def is a special for that associates a name to a value"
+  )
 
 (add-slide "doing more than one expression
 with do.
@@ -91,28 +158,28 @@ main use -> side effects
 returns last value of expression")
 
 (comment
-         ((println "hello") (println "bye"))
-         (do (println "hello") (println "bye"))
+  ((println "hello") (println "bye"))
+  (do (println "hello") (println "bye"))
 
-         )
+  )
 
 (add-slide "let it be... local names")
 
 (comment
-        (def x 10)
-        (let [x 2] (* x x))
-        (inc x)
-        )
+  (def x 10)
+  (let [x 2] (* x x))
+  (inc x)
+  )
 
 (add-slide "from let to function fn")
 
 (comment
-         (fn [x] (* x x))
-         #(* %1 %1)
-         )
+  (fn [x] (* x x))
+  #(* %1 %1)
+  )
 
 (add-slide  "naming a function")
 
 (comment
-         (def square (fn [x] (* x x)))
-         (defn square [x] (* x x)))
+  (def square (fn [x] (* x x)))
+  (defn square [x] (* x x)))
